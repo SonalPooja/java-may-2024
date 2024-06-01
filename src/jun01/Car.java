cc1de2a862cf00576e760d71453dbc3817c08341
Sonal-Pooja <sonalpooja11@gmail.com>
2024-06-01 11:51:30 -0400

    Adding new files with classes and objects

---
+++ src/jun01/Car.java
@@ -0,0 +1,31 @@
+package jun01;
+
+public class Car {
+
+    //properties
+    String color = "Blue";
+    String brand = "Tesla";
+    int engineSize = 200;
+
+    //behaviour
+    //methods - set of block statements which perform specific task.
+    // we can call a method whenever we want.
+    /*
+        Syntax for method:-
+
+        return_type method_name() {
+            return x;
+        }
+    */
+    void brake(){
+        System.out.println("Car brakes...");
+    }
+
+    void run(){
+        System.out.println("Car is running...");
+    }
+
+    boolean hasElectricEngine(){
+        return true;
+    }
+}
