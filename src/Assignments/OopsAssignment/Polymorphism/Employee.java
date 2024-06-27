f5e8e77c7dadaa45c4e728e5e40a2759d4da84f9
Sonal-Pooja <sonalpooja11@gmail.com>
2024-06-26 21:55:29 -0400

    polymorphism assignment with user input

--- src/Assignments/OopsAssignment/Polymorphism/Employee.java
+++ src/Assignments/OopsAssignment/Polymorphism/Employee.java
@@ -1,12 +1,15 @@
 package Assignments.OopsAssignment.Polymorphism;
 
+import java.util.Scanner;
+
 public class Employee {
 
     private int employeeId;
     private String name;
 
-    public double calculatePay(){
-        return 0;
+    public Employee(int employeeId, String name) {
+        this.employeeId = employeeId;
+        this.name = name;
     }
 
     public void displayDetails() {
@@ -15,8 +18,43 @@ public class Employee {
         System.out.println("Pay: $" + calculatePay());
     }
 
-    public Employee(int employeeId, String name) {
-        this.employeeId = employeeId;
-        this.name = name;
+    public double calculatePay(){
+        return 0;
+    }
+
+    public static void userInput(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter employee Id:");
+        int id = sc.nextInt();
+        System.out.println("Enter employee name:");
+        String name = sc.next();
+        System.out.println("Choose employee type: full-time or part-time");
+        String type = sc.next();
+
+        if(type.equals("full-time")){
+            System.out.println("Enter annual salary:");
+            double salary = sc.nextDouble();
+
+            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(id, name, salary);
+            System.out.println();
+            System.out.println("Full-time employee details: ");
+            fullTimeEmployee.displayDetails();
+            System.out.println();
+        }
+        else if (type.equals("part-time")){
+            System.out.println("Enter total number of hours worked:");
+            double hours = sc.nextDouble();
+            System.out.println("Enter hourly pay rate:");
+            double hourlyRate = sc.nextDouble();
+
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee(id, name, hours, hourlyRate);
+            System.out.println();
+            System.out.println("Part-time employee details: ");
+            partTimeEmployee.displayDetails();
+        }
+        else {
+            System.out.println("Enter a either full-time or part-time.");
+        }
     }
 }
--- src/Assignments/OopsAssignment/Polymorphism/EmployeeMain.java
+++ src/Assignments/OopsAssignment/Polymorphism/EmployeeMain.java
@@ -1,16 +1,11 @@
 package Assignments.OopsAssignment.Polymorphism;
 
+import java.util.Scanner;
+
 public class EmployeeMain {
 
     public static void main(String[] args) {
 
-        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(101, "Sonal", 78000);
-        System.out.println("Full-time employee details: ");
-        fullTimeEmployee.displayDetails();
-        System.out.println();
-
-        PartTimeEmployee partTimeEmployee = new PartTimeEmployee(102, "Pooja", 155, 25.5);
-        System.out.println("Part-time employee details: ");
-        partTimeEmployee.displayDetails();
+        Employee.userInput();
     }
 }
